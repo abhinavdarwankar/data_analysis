edeb71b695b9dbb6c776bceec72325048a096b80
abhinavdarwankar <abhinavdarwankar4@gmail.com>
2025-01-30 18:26:18 +0530

    new change

--- day_4/pie.py
+++ day_4/pie.py
@@ -2,6 +2,6 @@ import matplotlib .pyplot as plt
  
 subjects= ["DSA","Web development","SAD","AI"]
 marks=[90,80,70,60]
-plt.pie(marks,labels=subjects, startangle=90)
+plt.pie(marks,labels=subjects,)
 plt.title("marks in different subject")
 plt.show()
